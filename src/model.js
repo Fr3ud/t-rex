4209ff09df3f05234d702438a1f48686f1bc553d
Fr3ud <oo.pavelko@gmail.com>
2021-12-05 20:53:49 +0200

    add particles

--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { MeshSurfaceSampler  } from 'three/examples/jsm/math/MeshSurfaceSampler';
 
 export default class Model {
   constructor({ name, file, scene }) {
@@ -19,13 +20,39 @@ export default class Model {
   init() {
     this.loader.load(this.file, response => {
       this.mesh = response.scene.children[0];
+
       this.material = new THREE.MeshBasicMaterial({
         color: 'tomato',
         wireframe: true,
       });
       this.mesh.material = this.material;
+      this.geometry = this.mesh.geometry;
 
-      this.scene.add(this.mesh);
+      this.particlesMaterial = new THREE.PointsMaterial({
+        color: 'tomato',
+        size : 0.02,
+      })
+      const numParticles = 25000;
+      const sampler = new MeshSurfaceSampler(this.mesh).build();
+      this.particlesGeometry = new THREE.BufferGeometry();
+      const particlesPosition = new Float32Array(numParticles * 3)
+
+      for (let i = 0; i < numParticles; i++) {
+        const newPosition = new THREE.Vector3();
+
+        sampler.sample(newPosition);
+        particlesPosition.set([
+          newPosition.x,
+          newPosition.y,
+          newPosition.z,
+        ], i * 3);
+
+        this.particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlesPosition, 3))
+      }
+
+      this.particles = new THREE.Points(this.particlesGeometry, this.particlesMaterial)
+
+      this.scene.add(this.particles);
     });
   }
 }
