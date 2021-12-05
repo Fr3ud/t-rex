445aef457e64723b456037dc38ae7384742147f4
Fr3ud <oo.pavelko@gmail.com>
2021-12-05 20:08:01 +0200

    add tRex

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import Model from './model';
 
 /*------------------------------
 Renderer
 ------------------------------*/
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
-  alpha: true
+  alpha    : true,
 });
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
 
 
@@ -16,11 +19,11 @@ document.body.appendChild( renderer.domElement );
 Scene & Camera
 ------------------------------*/
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 
-  50, 
+const camera = new THREE.PerspectiveCamera(
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
-  100
+  100,
 );
 camera.position.z = 5;
 camera.position.y = 1;
@@ -29,12 +32,12 @@ camera.position.y = 1;
 /*------------------------------
 Mesh
 ------------------------------*/
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial( { 
-  color: 0x00ff00,
-} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry(2, 2, 2);
+// const material = new THREE.MeshBasicMaterial( {
+//   color: 0x00ff00,
+// } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
 
 /*------------------------------
@@ -48,9 +51,19 @@ Helpers
 ------------------------------*/
 const gridHelper = new THREE.GridHelper( 10, 10 );
 scene.add( gridHelper );
+
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
 
+
+/*------------------------------
+Models
+------------------------------*/
+const tRex = new Model({
+  name : 't-rex',
+  file : './models/t-rex.glb',
+  scene: scene,
+})
 
 /*------------------------------
 Loop
@@ -59,6 +72,7 @@ const animate = function () {
   requestAnimationFrame( animate );
   renderer.render( scene, camera );
 };
+
 animate();
 
 
@@ -68,6 +82,8 @@ Resize
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
+
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
+
 window.addEventListener( 'resize', onWindowResize, false );
